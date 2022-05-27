0d9991161d4b01918ffc18c4600168370339790b
Fernanda Zúniga <48185765+Zumari@users.noreply.github.com>
2022-05-27 16:38:44 -0600

    solucionar problema

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,25 +1,65 @@
-const getUsuarios = (req, res) => {
+const { response } = require('express');
+const { validationResult } = require('express-validator');
 
+const Usuario = require('../models/usuario'); 
+
+
+const getUsuarios = async(req, res) => {
+
+    const usuarios = await Usuario.find({}, 'nombre email role google');
+    
     res.status(200).json({
         ok: true,
-        msg: 'get Usuarios'
+        usuarios
     })
 
 }
 
-const crarUsuarios = (req, res) => {
+const crearUsuarios = async(req, res = response) => {
 
     // Como leer el body
-    console.log( req.body );
+    // console.log( req.body ); 
+    const { email, password, nombre } = req.body;
 
-    res.status(200).json({
-        ok: true,
-        msg: 'creando usuarios'
-    })
+    const errors = validationResult( req );
+
+    if ( !errors.isEmpty ){
+        return res.status(400).json({
+            errors: errors.array(),
+            ok: false,
+            errors: errors.mapped()
+        });
+    }
+
+    try {
+
+        const existeEmail = await Usuario.findOne({ email });
+
+        
+
+
+        const usuario = new Usuario( req.body );
+
+        await usuario.save();
+
+        res.json({
+            ok: true,
+            usuario
+        });
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'error inesperado... revisar logs'
+        });
+    }
+
+    
 
 }
 
 module.exports = {
     getUsuarios,
-    crarUsuarios,
+    crearUsuarios,
 }
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -3,12 +3,22 @@
 */
 
 const { Router } = require('express');
-const { getUsuarios, crarUsuarios } = require('../controllers/usuarios')
+const { check, body } = require('express-validator');
+
+const { getUsuarios, crearUsuarios } = require('../controllers/usuarios')
 
 const router = Router();
 
 router.get( '/', getUsuarios );
-router.post( '/', crarUsuarios ); // Crear usuario
+
+router.post( '/', 
+    [
+        body('nombre', 'El nombre es obligatorio').not().isEmpty(),
+        body('password', 'El password es obligatorio').not().isEmpty(),
+        body('email', 'La contraseña es obligatoria').isEmail(),
+    ], 
+    crearUsuarios 
+); // Crear usuario
 
 
 
